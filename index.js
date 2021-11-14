3d20b8f5b4a7fdd61f65cce38d21eb295067098a
agunbuhori <agun.buhori@gmail.com>
2021-11-14 22:59:37 +0700

    be updated

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ const ScheduleHandler = require('./handler/ScheduleHandler')
 require('dotenv').config()
 
 app.use(bodyParser())
-app.use(cors())
+
+var whitelist = ['https://kjr.kampustsl.id', 'http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+ 
+app.use(cors(corsOptions))
 
 app.use('/user', UserHandler)
 app.use('/schedule', ScheduleHandler)
