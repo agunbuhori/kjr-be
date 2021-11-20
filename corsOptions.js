a136e0b6d5e41421512bd64f92cbd03c2b4a692a
agunbuhori <agun.buhori@gmail.com>
2021-11-21 06:14:15 +0700

    status changed

--- corsOptions.js
+++ corsOptions.js
@@ -2,11 +2,8 @@ var whitelist = ['https://kjr.kampustsl.id', 'http://localhost:3000', undefined]
 
 var corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS '))
-    }
+    callback(null, true)
+
   }
 }
 
