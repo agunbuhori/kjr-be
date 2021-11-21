90b048bf12b33861a4a316993d01a06794afe67e
agunbuhori <agun.buhori@gmail.com>
2021-11-21 14:45:16 +0700

    updated fully

--- handler/UserHandler.js
+++ handler/UserHandler.js
@@ -8,6 +8,7 @@ const { makeid } = require('../helpers')
 const responseHandler = require('./responseHandler')
 const errorHandler = require('./errorHandler')
 const QRCode = require('qrcode')
+const mailer = require('../config/mailer')
 
 UserHandler.use(cors(corsOptions))
 
@@ -56,13 +57,63 @@ function getSchedule(slug) {
   })
 }
 
+function sendMail(target, data, attachments) {
+  mailer.sendMail({
+    from: 'support@kampustsl.id',
+    to: target,
+    subject: `Bukti Pendaftaran Kajian Rutin ${data.schedule.name}`,
+    html: `<p>
+بسم الله <br/>
+Ahlan ${data.user.name} <br/>
+Berikut QR Code dan bukti pendaftaran untuk <strong>${data.schedule.name}</strong><br/>
+Tempat : ${data.schedule.location}<br/>
+Tanggal : ${data.schedule.datetime}<br/>
+</p>
+
+<p>
+Silahkan simpan dan tunjukan QR Code ini pada panitia kajian. <br/>
+بارك الله فيكم 
+</p>
+
+<p>
+<strong>Catatan :</strong><br/>
+1. QR Code ini hanya untuk satu orang pendaftar.<br/>
+2. Mari jaga dan lakukan protokol kesehatan.
+</p>
+
+<p>
+Panitia Pendaftaran Kajian  Rutin<br/>
+Yayasan Tarbiyah Sunnah.<br/>
+Helpdesk wa.me/62895377710900
+</p>
+    `
+  })
+}
+
 UserHandler.get('/:id', (req, res) => {
   User.findById(req.params.id).then(async (user) => {
     const qrcode = await getQR(user._id.toString())
     const schedule = await getSchedule(user.schedule_id)
     let other = await User.findOne({email: user.email, schedule_id: user.schedule_id, name: {$ne: user.name}}).exec()
-    if (other)
-      other = {...other.toObject(), qrcode: await getQR(other._id.toString())}
+
+    const attachments = [
+      {
+        filename: user.name.concat('.png'),
+        path: qrcode
+      }
+    ]
+
+    if (other) {
+      const other_qrcode = await getQR(other._id.toString())
+      other = {...other.toObject(), qrcode: other_qrcode}
+      attachments.push({
+        filename: other.name.concat('.png'),
+        path: other_qrcode
+      })
+    }
+
+    sendMail(user.email, {schedule, user}, attachments)
+
     res.send(responseHandler({...user.toObject(), qrcode, schedule, other}))
   }).catch(err => {
     res.status(404).send(errorHandler(err))
