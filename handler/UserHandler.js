6a595d1284d5894c6f7f36129ca58dfdde9e5d1b
agunbuhori <agun.buhori@gmail.com>
2021-11-23 03:18:58 +0700

    mail must be confirmed

--- handler/UserHandler.js
+++ handler/UserHandler.js
@@ -17,7 +17,10 @@ function authorize(req, res, next) {
   if (req.headers.Authorization == 'Bearer bla bla bla') {
     next();
   } else {
-    res.status(403).send("Unauthorized");
+    res.status(403).send({
+      status: 403,
+      message: "Unauthorized"
+    });
   }
 }
 
