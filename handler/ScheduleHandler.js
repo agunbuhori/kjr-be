981389b2e5bf617f9194e37f1fcaeb256f069b47
agunbuhori <agun.buhori@gmail.com>
2021-11-23 20:12:22 +0700

    updated

--- handler/ScheduleHandler.js
+++ handler/ScheduleHandler.js
@@ -7,18 +7,30 @@ const cors = require('cors')
 const corsOptions = require('../corsOptions')
 const { getSlug } = require('../helpers')
 const authorize = require('../config/authorize')
+const UserModel = require('../models/User')
 
 const corsMiddleware = cors(corsOptions)
 
-ScheduleHandler.get('/list', authorize, corsMiddleware, (req, res) => {
-  ScheduleModel.find({}, [], {sort: {datetime: 1}}, (err, result) => {
+ScheduleHandler.get('/list', authorize, (req, res) => {
+  ScheduleModel.find({}, [], { sort: { datetime: 1 } }, (err, result) => {
     if (err) res.send(errorHandler(err))
 
     res.send(responseHandler(result))
   })
 })
 
-ScheduleHandler.post('/create', authorize, corsMiddleware, (req, res) => {
+ScheduleHandler.get('/channel/:id', authorize, (req, res) => {
+  ScheduleModel.findById(req.params.id, async (err, result) => {
+    let males = await UserModel.find({schedule_id: result.slug, gender: 'Ikhwan'}).count()
+    let males_present = await UserModel.find({schedule_id: result.slug, gender: 'Ikhwan', present: {$ne: null}}).count()
+    let females = await UserModel.find({schedule_id: result.slug, gender: 'Akhwat'}).count()
+    let females_present = await UserModel.find({schedule_id: result.slug, gender: 'Akhwat', present: {$ne: null}}).count()
+    
+    res.send(responseHandler({ males, females, males_present, females_present }))
+  })
+})
+
+ScheduleHandler.post('/create', authorize, (req, res) => {
   const times = new Date(req.body.datetime)
   const schedule = new ScheduleModel({ ...req.body, slug: getSlug(times) })
 
@@ -29,7 +41,7 @@ ScheduleHandler.post('/create', authorize, corsMiddleware, (req, res) => {
 })
 
 ScheduleHandler.get('/:slug', corsMiddleware, (req, res) => {
-  ScheduleModel.findOne({ slug: req.params.slug}, (err, result) => {
+  ScheduleModel.findOne({ slug: req.params.slug }, (err, result) => {
     if (err || !result) res.send(errorHandler(err))
 
     res.send(responseHandler(result))
@@ -43,6 +55,5 @@ ScheduleHandler.post('/authorize/:id', authorize, (req, res) => {
     res.send(responseHandler(result))
   })
 })
-
 
 module.exports = ScheduleHandler
--- handler/UserHandler.js
+++ handler/UserHandler.js
@@ -14,7 +14,6 @@ const authorize = require('../config/authorize')
 
 UserHandler.use(cors(corsOptions))
 
-
 UserHandler.get('/list', authorize, (req, res) => {
   User.find({}, ['-_id', 'name', 'created_at', 'mail_confirmed', 'wa_confirmed', 'present', 'device'], (err, result) => res.send(responseHandler(result)))
 })
@@ -24,38 +23,38 @@ UserHandler.get('/tc/f53180a7bb484bee15d7a2ffe40ae6fd', authorize, (req, res) =>
 })
 
 UserHandler.post('/register', async (req, res) => {
-  const check = await User.find({schedule_id: req.body.schedule_id, email: req.body.email}).exec()
+  const check = await User.find({ schedule_id: req.body.schedule_id, email: req.body.email }).exec()
 
   if (check.length === 0) {
-    const newUser = new User({...req.body, code: makeid(5), name: toTitleCase(req.body.name)})
+    const newUser = new User({ ...req.body, code: makeid(5), name: toTitleCase(req.body.name) })
     const newUserSave = await newUser.save()
 
     if (newUserSave && req.body.name_2 && req.body.age_2 && req.body.gender_2) {
-      const {name_2, age_2, gender_2} = req.body
-      const otherUser = new User({...req.body, code: makeid(5), name: toTitleCase(name_2), age: age_2, gender: gender_2})
+      const { name_2, age_2, gender_2 } = req.body
+      const otherUser = new User({ ...req.body, code: makeid(5), name: toTitleCase(name_2), age: age_2, gender: gender_2 })
       const otherUserSave = await otherUser.save()
 
-      res.send(responseHandler({...newUserSave.toObject(), other: otherUserSave.toObject()}))
+      res.send(responseHandler({ ...newUserSave.toObject(), other: otherUserSave.toObject() }))
     }
 
     res.send(responseHandler(newUserSave))
   } else {
-    res.send(errorHandler("Antum sudah melakukan registrasi"))
+    res.send(errorHandler('Antum sudah melakukan registrasi'))
   }
 })
 
 function getQR(string) {
   return new Promise((resolve, reject) => {
-    QRCode.toDataURL(string, {type: 'terminal'}, (err, src) => {
+    QRCode.toDataURL(string, { type: 'terminal' }, (err, src) => {
       if (err) reject(err)
       resolve(src)
     })
-  });
+  })
 }
 
 function getSchedule(slug) {
   return new Promise((resolve, reject) => {
-    Schedule.findOne({slug}, (err, result) => {
+    Schedule.findOne({ slug }, (err, result) => {
       if (err) reject(err)
       resolve(result)
     })
@@ -90,78 +89,96 @@ function sendMail(target, data, attachments = [], other = null) {
     Yayasan Tarbiyah Sunnah.<br/>
     Helpdesk wa.me/62895377710900
     </p>
-        `;
+        `
     if (other) {
-      template = template.replace(/\{OTHER\}/, `
+      template = template.replace(
+        /\{OTHER\}/,
+        `
         <p>Tiket ini terdaftar juga atas nama <strong>${other.name}.</strong></p>
-      `);
+      `
+      )
     } else {
-      template = template.replace(/\{OTHER\}/, '');
+      template = template.replace(/\{OTHER\}/, '')
     }
-    mailer.sendMail({
-      from: 'support@kampustsl.id',
-      to: target,
-      attachments,
-      subject: `Bukti Pendaftaran Kajian Rutin ${data.schedule.name}`,
-      html: template
-    }).then(async (err, sent) => {
-      await User.updateMany({email: data.user.email, schedule_id: data.user.schedule_id}, {mail_confirmed: (new Date())})
-      resolve(sent)
-    })
+    mailer
+      .sendMail({
+        from: 'support@kampustsl.id',
+        to: target,
+        attachments,
+        subject: `Bukti Pendaftaran Kajian Rutin ${data.schedule.name}`,
+        html: template,
+      })
+      .then(async (err, sent) => {
+        await User.updateMany({ email: data.user.email, schedule_id: data.user.schedule_id }, { mail_confirmed: new Date() })
+        resolve(sent)
+      })
   })
 }
 
+async function sendWa(user) {
+  await User.updateMany({ email: user.email, schedule_id: user.schedule_id }).exec()
+}
+
 UserHandler.get('/:id', (req, res) => {
-  User.findById(req.params.id).then(async (user) => {
-    const qrcode = await getQR(user._id.toString())
-    const schedule = await getSchedule(user.schedule_id)
-    let other = await User.findOne({email: user.email, schedule_id: user.schedule_id, code: {$ne: user.code}}).exec()
+  User.findById(req.params.id)
+    .then(async (user) => {
+      const qrcode = await getQR(user._id.toString())
+      const schedule = await getSchedule(user.schedule_id)
+      let other = await User.findOne({ email: user.email, schedule_id: user.schedule_id, code: { $ne: user.code } }).exec()
 
-    const attachments = [
-      {
-        filename: user.code + ' ' + user.name.concat('.png'),
-        path: qrcode
+      const attachments = [
+        {
+          filename: user.code + ' ' + user.name.concat('.png'),
+          path: qrcode,
+        },
+      ]
+
+      if (other) {
+        const other_qrcode = await getQR(other._id.toString())
+        other = { ...other.toObject(), qrcode: other_qrcode }
+        attachments.push({
+          filename: other.code + ' ' + other.name.concat('.png'),
+          path: other_qrcode,
+        })
       }
-    ]
 
-    if (other) {
-      const other_qrcode = await getQR(other._id.toString())
-      other = {...other.toObject(), qrcode: other_qrcode}
-      attachments.push({
-        filename: other.code + ' ' + other.name.concat('.png'),
-        path: other_qrcode
-      })
-    }
+      if (!user.mail_confirmed) {
+        sendMail(user.email, { schedule, user }, attachments, other)
+      }
 
-    if (! user.mail_confirmed) {
-      sendMail(user.email, {schedule, user}, attachments, other)
-    }
+      if (!user.wa_confirmed) {
+        sendWa(schedule, user, other)
+      }
 
-    res.send(responseHandler({...user.toObject(), qrcode, schedule, other}))
-  }).catch(err => {
-    res.status(404).send(errorHandler(err))
-  })
+      res.send(responseHandler({ ...user.toObject(), qrcode, schedule, other }))
+    })
+    .catch((err) => {
+      res.status(404).send(errorHandler(err))
+    })
 })
 
 UserHandler.post('/scan/:id', authorize, (req, res) => {
-  User.findById(req.params.id).then(async (user) => {
-    await User.findByIdAndUpdate(req.params.id, {present: (new Date), device: req.body.device});
+  User.findById(req.params.id)
+    .then(async (user) => {
+      await User.findByIdAndUpdate(req.params.id, { present: new Date(), device: req.body.device })
 
-    res.send(responseHandler(user));
-  }).catch(err => {
-    res.status(404).send(errorHandler(err));
-  })
-});
+      res.send(responseHandler(user))
+    })
+    .catch((err) => {
+      res.status(404).send(errorHandler(err))
+    })
+})
 
 UserHandler.post('/code-scan', authorize, (req, res) => {
-  User.findOne({code: req.body.code, schedule_id: req.body.schedule_id}).then(async (user) => {
-    await User.findOneAndUpdate({code: req.body.code, schedule_id: req.body.schedule_id}, {present: (new Date), device: req.body.device});
+  User.findOne({ code: req.body.code, schedule_id: req.body.schedule_id })
+    .then(async (user) => {
+      await User.findOneAndUpdate({ code: req.body.code, schedule_id: req.body.schedule_id }, { present: new Date(), device: req.body.device })
 
-    res.send(responseHandler(user));
-  }).catch(err => {
-    res.status(404).send(errorHandler(err));
-  })
-});
-
+      res.send(responseHandler(user))
+    })
+    .catch((err) => {
+      res.status(404).send(errorHandler(err))
+    })
+})
 
 module.exports = UserHandler
